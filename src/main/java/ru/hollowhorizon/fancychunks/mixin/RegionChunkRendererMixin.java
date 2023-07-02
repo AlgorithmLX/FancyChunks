b2ebb636cfa24274d14f8aacc4e51bd2de9cab6d
Ivan <46783751+AlgorithmLX@users.noreply.github.com>
2023-07-02 17:13:18 +0700

    Try to fix with #2

--- src/main/java/ru/hollowhorizon/fancychunks/mixin/RegionChunkRendererMixin.java
+++ src/main/java/ru/hollowhorizon/fancychunks/mixin/RegionChunkRendererMixin.java
@@ -17,17 +17,24 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-@Mixin(value = RegionChunkRenderer.class, remap = false)
+@Mixin(value = RegionChunkRenderer.class, remap = false, priority = 1)
 public class RegionChunkRendererMixin {
     public RegionChunkRendererMixin() {
     }
 
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/RegionChunkRenderer;setModelMatrixUniforms(Lme/jellysquid/mods/sodium/client/render/chunk/shader/ChunkShaderInterface;Lme/jellysquid/mods/sodium/client/render/chunk/region/RenderRegion;Lme/jellysquid/mods/sodium/client/render/chunk/ChunkCameraContext;)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILHARD)
+    @Inject(
+            method = "render",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lme/jellysquid/mods/sodium/client/render/chunk/RegionChunkRenderer;setModelMatrixUniforms(Lme/jellysquid/mods/sodium/client/render/chunk/shader/ChunkShaderInterface;Lme/jellysquid/mods/sodium/client/render/chunk/region/RenderRegion;Lme/jellysquid/mods/sodium/client/render/chunk/ChunkCameraContext;)V",
+                    shift = At.Shift.AFTER
+            ),
+            locals = LocalCapture.CAPTURE_FAILHARD
+    )
     private void modifyChunkRender(ChunkRenderMatrices matrices, CommandList commandList, ChunkRenderList list, BlockRenderPass pass, ChunkCameraContext camera, CallbackInfo ci, ChunkShaderInterface shader, Iterator i, Map.Entry e, RenderRegion region, List<RenderSection> chunks) {
         ChunkShaderInterfaceExt ext = (ChunkShaderInterfaceExt) shader;
         RenderRegionExt regionExt = (RenderRegionExt) region;
         regionExt.updateChunksFade(chunks, ext, commandList);
-
     }
 }
 
